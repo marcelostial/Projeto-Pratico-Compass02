1aa9bd6ccdf18366a685bc8ee5ba877bfd65b504
hiimarcelo <marceloaface@outlook.com>
2022-03-02 12:04:35 -0300

    Feat: finalizando estruturação da homePage

---
+++ src/views/homePage/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header/>
+    <HomePageContent/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from '@/containers/header/index.vue'
+import HomePageContent from '@/containers/homePageContent/index.vue'
+import Footer from '@/containers/footer/index.vue'
+export default {
+    name:"Home",
+    components: {
+      Header,
+      HomePageContent,
+      Footer,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
