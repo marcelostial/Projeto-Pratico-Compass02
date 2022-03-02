9b614f6a83610310f32d89ef446f620cae3b208f
hiimarcelo <marceloaface@outlook.com>
2022-03-02 12:19:00 -0300

    Feat: finalizando home header

---
+++ src/containers/header/index.vue
@@ -0,0 +1,28 @@
+.<template>
+    <div id="homeHeader">
+        <div id="logoHeader">
+         <img draggable="false" src="@/assets/logo-Compass-preta.png" alt="Loga Compass Preta"> 
+        </div>   
+        <div id="areaHorario">
+            <div id="hora">11:26</div>
+                <div id="data">Terça Feira, 17 de março de 2020</div>
+            </div>
+        <div id="areaTempo">
+             <div id="local"></div>
+                    <div id="climate">
+                        <img draggable="false" id="iconTempo" src="@/assets/cloud.png">
+                        <div id="grau">22°</div>
+                    </div>
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
