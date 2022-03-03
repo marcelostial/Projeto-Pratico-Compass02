f7f2b850f91e047ea5fe613bb143ad894a4063b6
hiimarcelo <marceloaface@outlook.com>
2022-03-03 13:25:47 -0300

    finalizando estrutura do footer

---
+++ src/components/timer/index.vue
@@ -0,0 +1,40 @@
+<template>
+<div id="nSecond" v-html="timerSeconds"/>
+</template>
+
+<script>
+import router from '../../router/index.js'
+
+export default {
+     data(){
+        return{
+            timerSeconds: "",
+        }
+    },
+
+    name: "Timer",
+
+    methods:{
+
+        timerLogical(){
+            let seconds = 600;
+            setInterval(() => {
+            this.timerSeconds = seconds;
+            seconds--
+
+            if (!seconds){
+                router.push({name:'Login'})
+                }
+            }, 1000);
+          }
+    },
+
+    created(){
+        this.timerLogical()
+    },
+}  
+</script>
+
+<style>
+
+</style>
---
+++ src/containers/footer/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="footer">
+                <div id="footerHome">
+                    <div id="textFooterArea">
+                        <p id="textFooter">
+                            Essa janela do navegador é usada para manter sua sessão de autenticação ativa. Deixe-a aberta em segundo plano e abra uma nova janela para continuar a navegar.
+                        </p>
+                    </div>
+                    <div id="barra"></div>
+                    <div id="areaContador">
+                        <p id="textContador"> Application refresh in</p>
+                        <div id="areaSeconds">
+                            <Timer/>
+                            <div id="tSecond">seconds</div>
+                        </div>
+                    </div>
+                    <button id="buttonNextNavigation">Continuar<br>Navegando</button>
+                    <button id="buttonLogout">Logout</button>
+                </div>
+    </div> 
+</template>
+
+<script>
+import Timer from "@/components/timer/index.vue";
+export default {
+    components: { Timer }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
