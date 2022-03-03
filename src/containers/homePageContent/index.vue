bea241cb439b7c2724374320e45e79dfcc156d75
hiimarcelo <marceloaface@outlook.com>
2022-03-03 11:05:58 -0300

    Fix: arrumando bug na pagina home

---
+++ src/containers/homePageContent/index.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="homeArea">
+                <div id="logoCompass">
+                    <img draggable="false" src="@/assets/bola-logo-Compass.png" alt="Bola Logo Compass">
+                </div>
+                <div id="areaText">
+                    <h3 id="homeTextEspecial1">Our mission is</h3>
+                    <p class="homeText2">Nossa missão é</p>
+                    <h1 class="homeText1">to transform the world</h1>
+                    <p class="homeText2">transformar o mundo</p>
+                    <h1 class="homeText1">building digital experiences</h1>
+                    <p class="homeText2">construindo experiências digitais</p>
+                    <h1 class="homeText1">that enabled our client's growth</h1>
+                    <p class="homeText2">que permitem o crescimento de nossos clientes</p>
+                </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
